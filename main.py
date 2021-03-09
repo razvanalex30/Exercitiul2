b92aae3a51643de6034cdd7d00f79bb67af678e8
RAlexandru <razvan_alex97@yahoo.com>
2021-03-09 17:56:52 +0200

    Functionality OK Version retrieved OK

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-
 driver = webdriver.Chrome('./chromedriver')
 driver.get('https://www.python.org/')
 # print(driver.title)
 menu = driver.find_element_by_id('downloads').click()
 
+version = ""
 
-try:
-    main = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='content']/div/section/div[1]/ol/li[1]")))
-    print(type(main.text))
-except:
-    driver.quit()
+main = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='content']/div/section/div[2]/ol")))
+articles = main.find_elements_by_tag_name("li")
+for article in articles:
+    header = article.find_element_by_class_name("release-number")
+    version = header.text
+    break
 
-
-
+print(version)
