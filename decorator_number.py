2ad14ff0b7c7fc835583943fe6332f5016604c7b
RAlexandru <razvan_alex97@yahoo.com>
2021-03-10 15:48:24 +0200

    Version_retrieve -> OK

--- decorator_number.py
+++ decorator_number.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver import ActionChains
+
 
 driver = webdriver.Chrome('./chromedriver')
 driver.get("https://www.python.org")
@@ -10,5 +12,11 @@ search_bar = driver.find_element_by_name("q")
 search_bar.clear()
 search_bar.send_keys("decorator")
 search_bar.send_keys(Keys.RETURN)
-results = WebDriverWait(driver, 10).until(ec.presence_of_element_located
-                                               ((By.XPATH, "//*[@id='content']/div/section/form/ul")))
+results = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.XPATH, "//ul[@class='list-recent-events "
+                                                                                    "menu']")))
+rezultate = driver.find_element_by_xpath("//a[text()='PEP 318 -- Decorators for Functions and Methods']")
+ac = ActionChains(driver)
+ac.move_to_element(rezultate).perform()
+ac.click(rezultate).perform()
+
+
--- version_retrieve.py
+++ version_retrieve.py
@@ -3,9 +3,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver import ActionChains
+import re
 
 
 class VersionRetrieve:
+
+    @staticmethod
+    def string_parse(input_str):
+        version_number = re.findall(r'[0-9]+.[0-9]+.[0-9]+', input_str)
+        return version_number[0]
 
     @classmethod
     def navigate(cls):
@@ -17,27 +23,25 @@ class VersionRetrieve:
     def find_path(cls):
         driver = cls.navigate()
         menu = driver.find_element_by_xpath(
-            "//ul[@class='navigation menu']//li[contains(@class,'tier-1 element-')]/a[text("
-            ")='Downloads']")
+            "//ul[@class='navigation menu']//li[contains(@class,'tier-1 element-')]/a[text()='Downloads']")
         ac = ActionChains(driver)
         ac.move_to_element(menu).perform()
         driver.find_element_by_xpath(
             "//ul[@class='navigation menu']//ul[@class='subnav menu']//li[contains(@class,'tier-2 element-')]//a["
             "text()='All releases']").click()
         return driver
+
     @classmethod
     def retrieve_version(cls):
-        driver=cls.find_path()
-        last_version = "The most recent Python version is:"
+        driver = cls.find_path()
+        last_version = "The most recent Python version is: "
         all_releases = WebDriverWait(driver, 10).until(ec.presence_of_element_located
                                                        ((By.XPATH, "//*[@id='content']/div/section/div[2]/ol")))
-        versions = all_releases.find_elements_by_tag_name("li")
-        for version in versions:
-            header = version.find_element_by_class_name("release-number")
-            last_version += header.text[6::]
-            break
+        version = all_releases.find_elements_by_tag_name("li")[0]
+        header = version.find_element_by_class_name("release-number")
+        version_number = cls.string_parse(header.text)
+        last_version += version_number
         return last_version
 
 
-# VersionRetrieve.navigate()
 print(VersionRetrieve.retrieve_version())
