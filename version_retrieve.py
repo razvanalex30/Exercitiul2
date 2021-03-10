a28d90a641778d9712f4fb669ff675ef6eb82402
RAlexandru <razvan_alex97@yahoo.com>
2021-03-10 09:52:31 +0200

    Class created and a class method to retrieve the latest version

--- version_retrieve.py
+++ version_retrieve.py
@@ -4,19 +4,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 
-driver = webdriver.Chrome('./chromedriver')
-driver.get('https://www.python.org/')
+class VersionRetrieve:
 
-menu = driver.find_element_by_id('downloads').click()
+    @classmethod
+    def retrieve_version(cls):
+        driver = webdriver.Chrome('./chromedriver')
+        driver.get('https://www.python.org/')
+        driver.find_element_by_id('downloads').click()
+        lastversion = ""
+        main = WebDriverWait(driver, 10).until(ec.presence_of_element_located
+                                               ((By.XPATH, "//*[@id='content']/div/section/div[2]/ol")))
+        versions = main.find_elements_by_tag_name("li")
+        for version in versions:
+            header = version.find_element_by_class_name("release-number")
+            lastversion = header.text
+            break
+        return lastversion
 
-version = ""
 
-main = WebDriverWait(driver, 10).until(ec.presence_of_element_located
-                                       ((By.XPATH, "//*[@id='content']/div/section/div[2]/ol")))
-articles = main.find_elements_by_tag_name("li")
-for article in articles:
-    header = article.find_element_by_class_name("release-number")
-    version = header.text
-    break
-
-print(version)
+print(VersionRetrieve.retrieve_version())
