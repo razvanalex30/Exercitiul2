dfb38a7fa31c00517aa68797a5b7dd6ae1efa018
RAlexandru <razvan_alex97@yahoo.com>
2021-03-12 12:29:41 +0200

    Decorator added for driver quit

--- decorator_number.py
+++ decorator_number.py
@@ -10,90 +10,80 @@ from functools import wraps
 
 def decorator_driver_quit(func):
     @wraps(func)
-    def wrapper():
-        print("The process has started!")
-        func()
+    def wrapper(*args, **kwargs):
+        driver = func(*args, **kwargs)
+        print("Process executed!")
+        driver.quit()
         print("Driver was stopped successfully!")
-        return func
+
     return wrapper
 
 
 class CheckExamples:
 
-    @classmethod
-    def navigate(cls):
+    def __init__(self):
+        self.driver = None
+
+    def navigate(self):
         """
         Method used to navigate to Python's Official Website
         :return: driver, necessary for completing other tasks on the webpage
         """
         chrome_options = Options()
         chrome_options.add_experimental_option("detach", True)
-        driver = webdriver.Chrome('./chromedriver', options=chrome_options)
-        driver.get('https://www.python.org/')
-        return driver
+        self.driver = webdriver.Chrome('./chromedriver', options=chrome_options)
+        self.driver.get('https://www.python.org/')
 
-    @classmethod
-    def search(cls):
+    def search(self):
         """
         Method used to search in the search bar of the site and press Enter to find results
         """
-        driver = cls.navigate()
-        search_bar = driver.find_element_by_name("q")
+        search_bar = self.driver.find_element_by_name("q")
         search_bar.clear()
         search_bar.send_keys("decorator")
         search_bar.send_keys(Keys.RETURN)
-        return driver
 
-    @classmethod
-    def get_first_result(cls):
+    def get_first_result(self):
         """
         Method used to click on the first result from the search list
         """
-        driver = cls.search()
-        results_view = WebDriverWait(driver, 10).until(
+        results_view = WebDriverWait(self.driver, 10).until(
             ec.presence_of_element_located((By.XPATH, "//ul[@class='list-recent-events "
                                                       "menu']")))
         first_result = results_view.find_element_by_xpath(
             "//a[text()='PEP 318 -- Decorators for Functions and Methods']")
-        ac = ActionChains(driver)
+        ac = ActionChains(self.driver)
         ac.move_to_element(first_result).click(first_result).perform()
-        return driver
 
-    @classmethod
-    def open_content_examples(cls):
+    def open_content_examples(self):
         """
         Method used to click on the Examples submenu from the Contents menu
         """
-        driver = cls.get_first_result()
-        content_view = WebDriverWait(driver, 10).until(
+        content_view = WebDriverWait(self.driver, 10).until(
             ec.presence_of_element_located((By.XPATH, "//article[@class='text']")))
         content_view_example = content_view.find_element_by_xpath("//ul[@class='simple']//a[text()='Examples']")
-        ac2 = ActionChains(driver)
+        ac2 = ActionChains(self.driver)
         ac2.move_to_element(content_view_example).click(content_view_example).perform()
-        return driver
 
-    @classmethod
-    def verify_example_count(cls):
+    @decorator_driver_quit
+    def verify_example_count(self):
         """
         Verify the number of examples presented in the Examples paragraph of the page
         :return: True if the number of examples in the Example sections is 5, False otherwise
         """
-        driver = cls.open_content_examples()
-        examples_view = driver.find_element_by_xpath("//div[@id='examples']//ol[@class='arabic']")
+        examples_view = self.driver.find_element_by_xpath("//div[@id='examples']//ol[@class='arabic']")
         examples_list = examples_view.find_elements_by_tag_name("li")
         if len(examples_list) == 5:
             print("Yes, there are {} examples".format(str(len(examples_list))))
 
         else:
             print("No, there are {} examples".format(str(len(examples_list))))
-        return driver
-
-    @staticmethod
-    @decorator_driver_quit
-    def driver_quit():
-        driver = CheckExamples.verify_example_count()
-        driver.quit()
-        print("The process was completed!")
+        return self.driver
 
 
-CheckExamples.driver_quit()
+a = CheckExamples()
+a.navigate()
+a.search()
+a.get_first_result()
+a.open_content_examples()
+a.verify_example_count()
