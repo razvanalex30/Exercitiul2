6fe1e7453221917239595ff55b4f0a33784030e3
RAlexandru <razvan_alex97@yahoo.com>
2021-03-15 12:29:02 +0200

    Version_retrieve -> Versions newer than 3.9 retrieved successfully

--- version_retrieve.py
+++ version_retrieve.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 # from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
-import datetime
+from datetime import datetime
 import re
 
 
@@ -15,6 +15,20 @@ class VersionRetrieve:
 
     def __init__(self):
         self.driver = None
+        self.first_release = None
+        self.last_active_version = None
+
+    @staticmethod
+    def date_parse_2(input_str):
+        date_obj = datetime.strptime(input_str, "%Y-%m-%d")
+        return date_obj
+
+    @staticmethod
+    def date_parse(input_str):
+        date_string = input_str.split(" ")
+        final_date = " ".join([date_string[0][0:3], date_string[1], date_string[2]])
+        date_obj = datetime.strptime(final_date, "%b %d, %Y")
+        return date_obj
 
     @staticmethod
     def string_parse(input_str):
@@ -54,7 +68,7 @@ class VersionRetrieve:
         Method used to retrieve the last python version from the table available in All releases
         """
         self.find_path()
-        # last_version = "The most recent Python version is: "
+        self.retrieve_table_version()
         all_releases = self.driver.find_element_by_xpath("//*[@id='content']/div/section/div[2]/ol")
         versions = all_releases.find_elements_by_tag_name("li")
         release_list = []
@@ -62,18 +76,27 @@ class VersionRetrieve:
             version_dict = {'release_version': None, 'release_date': None}
             header = version.find_element_by_class_name("release-number")
             header_2 = version.find_element_by_class_name("release-date")
-            # version_number = self.string_parse(header.text)
-            version_dict['release_version'] = header.text
-
-            # last_version += version_number
-            print(header.text)
+            version_dict['release_version'] = self.string_parse(header.text)
+            version_dict['release_date'] = self.date_parse(header_2.text)
+            if version_dict['release_date'] > self.first_release:
+                if self.last_active_version in version_dict['release_version']:
+                    release_list.append(version_dict)
+        if len(release_list) > 0:
+            print("Yes, there are {} versions newer than the {} version".format(len(release_list),
+                                                                                self.last_active_version))
+        else:
+            print("There are no versions newer than the {} version".format(self.last_active_version))
 
     def retrieve_table_version(self):
-        self.find_path()
         last_python_version = self.driver.find_element_by_xpath("//ol[@class='list-row-container menu']//li//span["
                                                                 "text()='3.9']")
         last_active_version = last_python_version.text
-        print(last_active_version)
+        self.last_active_version = last_active_version
+        release_active_version = self.driver.find_element_by_xpath("//ol[@class='list-row-container menu']//li//span["
+                                                                   "text()='2020-10-05']")
+        header = release_active_version.text
+        first_release = self.date_parse_2(header)
+        self.first_release = first_release
 
 
 version_last = VersionRetrieve()
